4154f058b5550f7b59bf7697469771424f1a8701
SocolAndreea <andreeas.2002@yahoo.com>
2024-03-28 17:43:17 +0200

    created user and items, made connection to db (app.module.ts), removed prettier errors(eslintrc)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'database',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule,
+    ItemModule,
+  ],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
